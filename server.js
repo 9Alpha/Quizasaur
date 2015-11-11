02413db2af3eb0be427168328d798374b9b1475e
Mitchell <maxwell.mitchell16@mbpr1315-01792.trinity.nyc.ny.us>
2015-11-11 14:38:19 -0500

    with ejs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var  express =  require('express');
 var  path =  require('path');
 var  app =  express();
+var ejs = require('ejs');
 
 var bodyParser = require('body-parser');  
 
@@ -8,13 +9,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var fs = require("fs");
-var content = fs.readFileSync("public/index.html", 'utf8');
+//var content = fs.readFileSync("public/index.html", 'utf8');
 
 
 app.use("/public", express.static(path.join(__dirname,'public')));
 
 app.get('/', function (req, res) {
-	res.send(content);
+	res.render('index.ejs', function() {
+        var quizes = JSON.parse(fs.readFileSync('data/allQuizes.json'));
+        var titles = [];
+        for (var i = 0; i < quizes.length; i++) {
+            titles.push(quizes[i].title);
+        }
+        return titles;
+    });
 });
 
 app.get('/quiz', function (req, res) {
